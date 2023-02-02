95b4fe1fdb4140dffe0d0776f10230d0f0769835
devloliconic <kirillevsmirnov@gmail.com>
2023-02-02 19:51:54 +0700

    feat: create delete position action

--- src/store/useOrderStore.ts
+++ src/store/useOrderStore.ts
@@ -13,12 +13,19 @@ export interface Order {
 interface OrderState {
   orders: Order[];
   addPosition: (position: Order) => void;
+  deletePosition: (id: number) => void;
 }
 
 export const useOrderStore = create<OrderState>((set) => ({
   orders: [],
+
   addPosition: (position: Order) =>
     set((state) => ({
       orders: [...state.orders, position],
+    })),
+
+  deletePosition: (id: number) =>
+    set((state) => ({
+      orders: [...state.orders.filter((it: Order) => it.id !== id)],
     })),
 }));
